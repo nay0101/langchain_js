7be3de4859f5d23c0a13c9b37d8513842ae0aa3d
nay010 <naylinaung.projects@gmail.com>
2024-03-15 13:25:50 +0630

    File Loader DOCX, PPTX Added

--- utils/fileloaders.js
+++ utils/fileloaders.js
@@ -2,12 +2,16 @@ import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { splitDocuments } from "./splitDocuments.js";
 import { TextLoader } from "langchain/document_loaders/fs/text";
+import { DocxLoader } from "langchain/document_loaders/fs/docx";
+import { PPTXLoader } from "langchain/document_loaders/fs/pptx";
 
 async function useDirectoryLoader(directory) {
   /* Load all PDFs within the specified directory */
   const directoryLoader = new DirectoryLoader(directory, {
     ".pdf": (path) => new PDFLoader(path),
     ".txt": (path) => new TextLoader(path),
+    ".docx": (path) => new DocxLoader(path),
+    ".pptx": (path) => new PPTXLoader(path),
   });
 
   const docs = await directoryLoader.load();
