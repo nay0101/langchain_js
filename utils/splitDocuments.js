944a736a9e904f195cf3b966115feaf28b937ded
nay010 <naylinaung.projects@gmail.com>
2024-03-20 15:37:09 +0630

    Mixtral Added

--- utils/splitDocuments.js
+++ utils/splitDocuments.js
@@ -1,6 +1,5 @@
 import { HtmlToTextTransformer } from "@langchain/community/document_transformers/html_to_text";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import * as fs from "node:fs";
 
 async function splitDocuments(docs) {
   const splitter = new RecursiveCharacterTextSplitter({
@@ -10,21 +9,6 @@ async function splitDocuments(docs) {
   const transformer = new HtmlToTextTransformer();
   const sequence = splitter.pipe(transformer);
   const documents = await sequence.invoke(docs.flat());
-
-  /* Getting Logs */
-  // fs.writeFile("./logs/dataloaderLog.txt", "", (err) => {
-  //   if (err) console.log(err);
-  // });
-
-  // documents.flat().forEach((d, i) => {
-  //   fs.appendFile(
-  //     "./logs/dataloaderLog.txt",
-  //     `\n${i}\n${d.metadata.source}\n${d.pageContent}\n`,
-  //     (err) => {
-  //       if (err) console.log(err);
-  //     }
-  //   );
-  // });
 
   return { documents };
 }
