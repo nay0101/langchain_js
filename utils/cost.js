1355e7b294177ce0386aa5b39abdb6a05d4f4bb1
nay010 <naylinaung.projects@gmail.com>
2024-10-24 15:00:21 +0700

    Cost function Update

--- utils/cost.js
+++ utils/cost.js
@@ -3,16 +3,22 @@ import { Langfuse } from "langfuse-langchain";
 
 config();
 
-async function getTotalCost({ chatbotName }) {
+async function getTotalCost({ chatbotName, fromTimestamp, toTimestamp }) {
   const langfuse = new Langfuse();
-  const totalPages = (await langfuse.fetchTraces({ userId: chatbotName })).meta
+  const langfuseConfig = {
+    userId: chatbotName,
+    fromTimestamp: fromTimestamp,
+    toTimestamp: toTimestamp,
+  };
+
+  const totalPages = (await langfuse.fetchTraces({ ...langfuseConfig })).meta
     .totalPages;
 
   let cost = 0;
   for (let pageNumber = 0; pageNumber < totalPages; pageNumber++) {
     const traces = await langfuse.fetchTraces({
-      userId: chatbotName,
       page: pageNumber + 1,
+      ...langfuseConfig,
     });
 
     traces.data.forEach((trace) => {
